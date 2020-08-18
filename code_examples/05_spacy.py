e9dd13280d92115330b653158404f56c05ee489c
Mauricio Collazos <ma0@contraslash.com>
2020-08-17 22:49:01 -0500

    examples for spacy

---
+++ code_examples/05_spacy.py
@@ -0,0 +1,10 @@
+# python -m spacy download en_core_web_sm
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
+phrase = "I love Ice Cream. I also like steak"
+document = nlp(phrase)
+
+for token in document:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
---
+++ code_examples/06_named_entity_recognition.py
@@ -0,0 +1,9 @@
+# python -m spacy download en_core_web_sm
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
+phrase = "I love Ice Cream. I also like steak"
+document = nlp(phrase)
+
+for entity in document.ents:
+    print(entity.text, entity.start_char, entity.end_char, entity.label_)
